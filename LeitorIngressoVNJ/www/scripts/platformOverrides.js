d8d79f3f30dfdc22a161c9e70af644617e9ffadc
LgiMatBrd <contato@skorp.com.br>
2017-10-31 12:11:58 -0200

    limpando

--- LeitorIngressoVNJ/www/scripts/platformOverrides.js
+++
@@ -1,4 +0,0 @@
-﻿/*
-	Este arquivo é substituído pelo código específico da plataforma da pasta /merges.
-	Obtenha mais informações em http://taco.visualstudio.com/pt-br/docs/configure-app/#Content.
-*/
