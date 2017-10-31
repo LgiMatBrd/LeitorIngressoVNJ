ab01bf726bd8f581d030dc1410191cbc9c29a4fd
LgiMatBrd <contato@skorp.com.br>
2017-10-31 18:00:27 -0200

    Tudo funcionando

--- www/app/app.js
+++ www/app/app.js
@@ -7,7 +7,7 @@ angular.module("ngapp", [ "ui.router", "ngMaterial", "ngCordova", "ngStorage" ])
   document.addEventListener("deviceready", function () {
     $cordovaStatusbar.overlaysWebView(false); // Always Show Status Bar
     $cordovaStatusbar.styleHex('#E53935'); // Status Bar With Red Color, Using Angular-Material Style
-    window.plugins.orientationLock.lock("portrait");
+    //window.plugins.orientationLock.lock("portrait");
   }, false);
   /* Hijack Android Back Button (You Can Set Different Functions for Each View by Checking the $state.current)
   document.addEventListener("backbutton", function (e) {
--- www/app/components/main/main-controller.js
+++ www/app/components/main/main-controller.js
@@ -1,57 +1,38 @@
-"use strict";
-
 angular.module("ngapp").controller("MainController", function(shared, $state, $scope, $mdSidenav, $mdComponentRegistry){
 
     var ctrl = this;
-
     this.auth = shared.info.auth;
-
     this.toggle = angular.noop;
-
     this.title = $state.current.title;
 
-    this.scan = function() {
-        var fName = "scan():";
-        console.log(fName, "entry");
-        try { 
-            if (window.tinyHippos) {
-                thirdPartyEmulator();
-                console.log(fName, "emulator alert");
-            } else {
-                cordova.plugins.barcodeScanner.scan(
-                    function (result) {
-                        //console.log(fName, "Leitura compelta!");
-                        //alert("Código recebido!\n" + "Resultado: " + result.text + "\n" + "Formato: " + result.format + "\n");
-                        //scan();
-                        $.post('https://amigo.basquetejoinville.com.br/retaguarda/rsocio/leituraIngresso/123', function(res){
-                            var resposta = JSON.parse(res);
-                            if (resposta[0].RESPOSTA == 0) {
-                                ctrl.mensagemRetornoTitulo = "Resposta:";
-                                ctrl.mensagemRetorno = resposta[0].MENSAGEM;
-                                //document.getElementById("mensagemResposta").innerHTML = resposta[0].MENSAGEM;
-                                //setTimeout(scan, 2000); 
-                            } else {
-                                ctrl.mensagemRetornoTitulo = "Resposta:";
-                                ctrl.mensagemRetorno = resposta[0].MENSAGEM;
-                                //document.getElementById("mensagemResposta").innerHTML = resposta[0].MENSAGEM;
-                                //setTimeout(scan, 2000);
-                            }
-                        });
-                    },
-                    function (error) {
-                        alert("Falha na leitura: " + error);
+    $scope.mensagemRetornoTitulo = "Resposta:";
+    $scope.mensagemRetorno = "";
+    
+    $scope.scan = function() {
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                //alert("Código recebido!\n" + "Resultado: " + result.text + "\n" + "Formato: " + result.format + "\n");
+                $.post('https://amigo.basquetejoinville.com.br/retaguarda/rsocio/leituraIngresso/' + result.text, function(res){
+                    var resposta = JSON.parse(res);
+                    console.dir(resposta);
+                    if (resposta[0].RESPOSTA == 0) {
+                        $scope.mensagemRetornoTitulo = "Resposta:";
+                        $scope.mensagemRetorno = resposta[0].MENSAGEM;
+                        setTimeout(scan, 2000); 
+                    } else {
+                        $scope.mensagemRetornoTitulo = "Resposta:";
+                        $scope.mensagemRetorno = resposta[0].MENSAGEM;
+                        setTimeout($scope.scan, 2000);
                     }
-                );
-            }
-        } catch (e) {
-            console.log(fName, "catch, failure");
-        }
-
-        $timeout(function() { ctrl.mensagemRetorno = ""; }, 2000);
-        console.log(fName, "exit");
+                });
+            },
+            function (error) {
+                alert("Falha na leitura: " + error);
+            });
     }
-
+    
     this.isOpen = function() { return false };
+    
     $mdComponentRegistry
     .when("left")
     .then( function(sideNav){
